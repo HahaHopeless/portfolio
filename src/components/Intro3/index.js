ec3fb5bc5934f5f8a675d8260f4f1c400a5fdfe3
RoddyGlitchh <mohsin.kamal.akbar@gmail.com>
2021-12-02 02:09:24 +0100

    a brave new world

--- src/components/Intro3/index.js
+++ src/components/Intro3/index.js
@@ -1,76 +1,133 @@
-import "./styles.css";
-import React, { useEffect } from "react";
-import { FormattedMessage } from "react-intl";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import gsap from "gsap";
-import Lottie from "react-lottie";
-import { Timeline } from "gsap/gsap-core";
+import React, { useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+import Matter from "matter-js";
 
 const Intro3 = (props) => {
+  const scene = useRef("scene");
   useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
-    var myTechStack = new Timeline();
+    var Engine = Matter.Engine,
+      Render = Matter.Render,
+      Runner = Matter.Runner,
+      Composites = Matter.Composites,
+      MouseConstraint = Matter.MouseConstraint,
+      Mouse = Matter.Mouse,
+      Composite = Matter.Composite,
+      Bodies = Matter.Bodies;
 
-    myTechStack.fromTo(
-      ".myTechStack",
-      { scaleY: 0, rotateZ: -90 },
-      {
-        scaleY: 1,
-        rotateZ: -90,
-        scrollTrigger: {
-          trigger: ".myTechStack",
-          toggleActions: "play none none reverse",
-          start: "top center",
-          end: "bottom center",
-          scrub: 1.5,
-          ease: "elastic",
-        },
-      }
-    );
+    // create engine
+    var engine = Engine.create(),
+      world = engine.world;
 
-    gsap.timeline({
-      scrollTrigger: {
-        trigger: ".hero3",
-        pin: ".hero3",
-        start: "center center",
-        end: "+=2000",
-        // scrub: 1,
-        // markers: true,
+    // create renderer
+    var render = Render.create({
+      element: scene.current,
+      engine: engine,
+      width: 1000,
+      height: 1000,
+      options: {
+        showAngleIndicator: false,
+        wireframes: false,
       },
     });
 
-    return () => {
-      ScrollTrigger.getAll().forEach((instance) => {
-        instance.kill();
+    Render.run(render);
+
+    // create runner
+    var runner = Runner.create();
+    Runner.run(runner, engine);
+
+    // add bodies
+    var stack = Composites.stack(
+      200,
+      606 - 25.25 - 5 * 40,
+      10,
+      5,
+      0,
+      0,
+      function (x, y) {
+        return Bodies.rectangle(x, y, 40, 40, {
+          render: {
+            fillStyle: "white",
+            strokeStyle: "blue",
+            lineWidth: 3,
+          },
+        });
+      }
+    );
+
+    Composite.add(world, [
+      stack,
+      // walls
+      Bodies.rectangle(400, 0, 800, 50, {
+        isStatic: true,
+        render: {
+          fillStyle: "white",
+          strokeStyle: "white",
+          lineWidth: 3,
+        },
+      }),
+      Bodies.rectangle(800, 300, 50, 600, {
+        isStatic: true,
+        render: {
+          fillStyle: "white",
+          strokeStyle: "white",
+          lineWidth: 3,
+        },
+      }),
+      Bodies.rectangle(0, 300, 50, 600, {
+        isStatic: true,
+        render: {
+          fillStyle: "white",
+          strokeStyle: "white",
+          lineWidth: 3,
+        },
+      }),
+      Bodies.rectangle(400, 606, 800, 50.5, {
+        isStatic: true,
+        render: {
+          fillStyle: "white",
+          strokeStyle: "white",
+          lineWidth: 3,
+        },
+      }),
+    ]);
+
+    // add mouse control
+    var mouse = Mouse.create(render.canvas),
+      mouseConstraint = MouseConstraint.create(engine, {
+        mouse: mouse,
+        constraint: {
+          stiffness: 0.2,
+          render: {
+            visible: false,
+          },
+        },
       });
-      gsap.killTweensOf(window);
+
+    Composite.add(world, mouseConstraint);
+
+    // keep the mouse in sync with rendering
+    render.mouse = mouse;
+
+    // fit the render viewport to the scene
+    Render.lookAt(render, {
+      min: { x: 0, y: 0 },
+      max: { x: 800, y: 600 },
+    });
+
+    // context for MatterTools.Demo
+    return {
+      engine: engine,
+      runner: runner,
+      render: render,
+      canvas: render.canvas,
+      stop: function () {
+        Matter.Render.stop(render);
+        Matter.Runner.stop(runner);
+      },
     };
   }, []);
 
-  return (
-    <section
-      style={{ height: "100vh" }}
-      className='hero3 vw-100 d-flex flex-column align-items-center justify-content-center overflow-hidden container-fluid '
-    >
-      <h1
-        className='myTechStack'
-        style={{
-          fontWeight: "800",
-          fontSize: "90px",
-          textShadow: "0px 0px 10px rgba(47,53,66,0.3)",
-          userSelect: "none",
-          textOverflow: "clip",
-          overflow: "hidden",
-          whiteSpace: "nowrap",
-          marginLeft: "-102vw",
-          marginBottom: "-30vh",
-          transformOrigin: "top",
-        }}
-      >
-        <FormattedMessage id='TECH STACK' />
-      </h1>
-    </section>
-  );
+  return <div ref={scene} className='scene' />;
 };
-
 export default Intro3;
